06fe5d738386d595507a91a62c569fd7d3e7af15
outtaspace <flyweight@yandex.ru>
2015-04-27 07:55:46 +0300

    initial revision

---
+++ lib/JIP/LockFile.pm
@@ -0,0 +1,206 @@
+package JIP::LockFile;
+
+use 5.006;
+use strict;
+use warnings;
+use IO::File;
+use Carp qw(croak);
+use Fcntl qw(LOCK_EX);
+use English qw(-no_match_vars);
+
+our $VERSION = '0.01';
+
+# http://search.cpan.org/~mlawren/Lock-Socket-0.0.6/lib/Lock/Socket.pm
+sub new {
+    my ($class, %param) = @ARG;
+
+    # Mandatory options
+    croak qq{Mandatory argument "lock_file" is missing\n}
+        unless exists $param{'lock_file'};
+
+    # Check "lock_file"
+    my $lock_file = $param{'lock_file'};
+    croak qq{Bad argument "lock_file"\n}
+        unless defined $lock_file and length $lock_file;
+
+    # Class to object
+    return bless({}, $class)
+        ->_set_is_locked(0)
+        ->_set_path_to_file($param{'lock_file'})
+        ->_set_fh(undef);
+}
+
+# Lock or raise an exception
+sub lock {
+    my $self = shift;
+
+    # Re-locking changes nothing
+    return $self if $self->_get_is_locked;
+
+    my $fh = IO::File->new($self->_get_path_to_file, O_WRONLY|O_CREAT)
+        or croak sprintf(qq{Can't open "%s": %s\n}, $self->_get_path_to_file, $OS_ERROR);
+
+    flock $fh, LOCK_EX
+        or croak sprintf(qq{Can't lock "%s": %s\n}, $OS_ERROR);
+
+    $fh->print($self->_lock_message())
+        or croak sprintf(qq{Can't write message to file: %s}, $OS_ERROR);
+
+    return $self->_set_fh($fh)->_set_is_locked(1);
+}
+
+# Or just return undef
+sub try_lock {
+    my $self = shift;
+
+    # Re-locking changes nothing
+    return $self if $self->_get_is_locked;
+
+    my $fh = IO::File->new($self->_get_path_to_file, O_WRONLY|O_CREAT);
+
+    if ($fh and flock $fh, LOCK_EX) {
+        $fh->print($self->_lock_message())
+            or croak sprintf(qq{Can't write message to file: %s}, $OS_ERROR);
+
+        return $self->_set_fh($fh)->_set_is_locked(1);
+    }
+    else {
+        return;
+    }
+}
+
+# But trying to get a lock is ok
+sub is_locked {
+    my $self = shift;
+    return $self->_get_is_locked;
+}
+
+# You can manually unlock
+sub unlock {
+    my $self = shift;
+
+    # Re-unlocking changes nothing
+    return $self if not $self->_get_is_locked;
+
+    # Close filehandle before file removing
+    unlink $self->_set_fh(undef)->_get_path_to_file
+        or croak sprintf(qq{Can't unlink "%s": %s\n}, $self->_get_path_to_file, $OS_ERROR);
+
+    $self->_set_is_locked(0);
+}
+
+sub _set_is_locked {
+    my ($self, $is_locked) = @ARG;
+    $self->{'is_locked'} = $is_locked;
+    return $self;
+}
+
+sub _get_is_locked {
+    my $self = shift;
+    return $self->{'is_locked'};
+}
+
+sub _set_fh {
+    my ($self, $fh) = @ARG;
+    $self->{'filehandle'} = $fh;
+    return $self;
+}
+
+sub _get_fh {
+    my $self = shift;
+    return $self->{'filehandle'};
+}
+
+sub _set_path_to_file {
+    my ($self, $path_to_file) = @ARG;
+    $self->{'path_to_file'} = $path_to_file;
+    return $self;
+}
+
+sub _get_path_to_file {
+    my $self = shift;
+    return $self->{'path_to_file'};
+}
+
+sub _lock_message {
+    return sprintf q[{"pid":"%s","executable_name":"%s"}],
+        $PROCESS_ID,
+        $EXECUTABLE_NAME;
+}
+
+1;
+
+=head1 NAME
+
+JIP::LockFile - application lock/mutex based on files
+
+=head1 VERSION
+
+Version 0.01
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use JIP::LockFile;
+
+    my $lock_file = './path/to/pid_file';
+
+    my $foo = JIP::LockFile->new(lock_file => $lock_file);
+    my $wtf = JIP::LockFile->new(lock_file => $lock_file);
+
+    $foo->lock;           # lock or raise exception
+    eval { $wtf->lock; }; # raise exception
+
+    $foo->lock; # Re-locking changes nothing
+
+
+=head1 AUTHOR
+
+Vladimir Zhavoronkov, C<< <flyweight at yandex.ru> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 Vladimir Zhavoronkov.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+=cut
+
---
+++ t/00-load.t
@@ -0,0 +1,15 @@
+#!perl -T
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+use Test::More;
+
+plan tests => 1;
+
+BEGIN {
+    use_ok('JIP::LockFile') || print "Bail out!\n";
+}
+
+diag("Testing JIP::LockFile $JIP::LockFile::VERSION, Perl $], $^X");
+
